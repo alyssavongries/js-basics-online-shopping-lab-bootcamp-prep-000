7c31f452e8b693e998d2202d0c5884b8929d44e0
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-23 22:24:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,9 +62,8 @@ function placeOrder(cardNumber) {
     if(cardNumber === undefined){
     return `Sorry, we don't have a credit card on file for you.`
   } else {
-    var price = total()
-
+    var price = total();
+    setCart();
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
       }
-  setCart();
 }
